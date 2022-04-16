c1ab7dc7be99987e98ea1c178e057358876a5066
Elnur Ahmadov <eelnur020@gmail.com>
2022-04-16 18:57:44 +0400

    Tests(FlightsInfoController) committed

--- src/main/java/com/company/dto/FlightsInfoDto.java
+++ src/main/java/com/company/dto/FlightsInfoDto.java
@@ -26,4 +26,6 @@ public class FlightsInfoDto {
 
     Long freeSeats;
 
+    public FlightsInfoDto(FlightsInfoDto flightsInfoDto) {
+    }
 }
---
+++ src/test/java/com/company/controller/UserControllerTest.java
@@ -0,0 +1,30 @@
+package com.company.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+@ExtendWith(MockitoExtension.class)
+class UserControllerTest {
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
