a4dab7d206deb9a4504df7e29fcd0ab6827642cd
Elnur Ahmadov <eelnur020@gmail.com>
2022-04-16 19:24:58 +0400

    Tests(UserServiceImpl) committed

---
+++ src/test/java/com/company/serviceImpl/UserServiceImplTest.java
@@ -0,0 +1,31 @@
+package com.company.serviceImpl;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceImplTest {
+    @ExtendWith(MockitoExtension.class)
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
